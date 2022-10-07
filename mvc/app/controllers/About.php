4e55ce3ed9fb00cac32bef19a037355d08cc8b9c
Anam140601 <khoerulanam140601@gmail.com>
2022-10-07 15:46:48 +0700

    4. Membuat View yang dapat diakses controller

--- mvc/app/controllers/About.php
+++ mvc/app/controllers/About.php
@@ -1,10 +1,23 @@
 <?php
 
-class About {
-   public function index($nama="Khoerul", $pekerjaan="Mahasiswa"){
-      echo "Halo, nama saya $nama. Saya seorang $pekerjaan";
+class About extends Controller{
+
+   public function index($nama="Khoerul", $pekerjaan="Mahasiswa", $umur=21){
+      $data['nama'] = $nama;
+      $data['pekerjaan'] = $pekerjaan;
+      $data['umur'] = $umur;
+
+      $data['judul'] = 'About Me';
+      $this->view('templates/header',$data);
+      $this->view('about/index', $data);
+      $this->view('templates/footer');
    }
+
    public function page(){
-      echo 'About/page';
+      $data['judul'] = 'Page';
+      $this->view('templates/header',$data);
+      $this->view('about/page');
+      $this->view('templates/footer');
    }
+
 }
---
+++ mvc/app/views/about/index.php
@@ -0,0 +1,3 @@
+
+   <h1>about me</h1>
+   <p>halo nama saya <?= $data['nama'] ?>, saya seorang <?= $data['pekerjaan'] ?> berumur <?= $data['umur'] ?> tahun</p>
