54513f36120481658716dd5dff654f5b51ead580
anigato <203040096@mail.unpas.ac.id>
2022-10-14 15:03:21 +0700

    Laravel 2. membuat route dan view, mengirimkan data ke view

--- LaravelANIGATO/routes/web.php
+++ LaravelANIGATO/routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// routing ke home
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+// routing ke about
+Route::get('/about', function () {
+    // Mengirimkan data ke view
+    return view('about',[
+        "name" => "Khoerul Anam",
+        "email" => "203040096@mail.unpas.ac.id",
+        "image" => "me.png"
+    ]);
+});
+
+// routing ke blog
+Route::get('/blog', function () {
+    return view('posts');
 });
