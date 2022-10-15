cc910ed0412d37e370a79d0f2d2e1be6f9e9d224
Anam140601 <khoerulanam140601@gmail.com>
2022-10-15 23:26:17 +0700

    Laravel 4. Blade templating simulasi blog post dan singgle post

--- LaravelANIGATO/routes/web.php
+++ LaravelANIGATO/routes/web.php
@@ -15,20 +15,75 @@ use Illuminate\Support\Facades\Route;
 
 // routing ke home
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 // routing ke about
 Route::get('/about', function () {
     // Mengirimkan data ke view
-    return view('about',[
-        "name" => "Khoerul Anam",
+    return view('about', [
+        "title" => "About",
+        "name"  => "Khoerul Anam",
         "email" => "203040096@mail.unpas.ac.id",
         "image" => "me.png"
     ]);
 });
 
+
+
 // routing ke blog
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title"  => "Hari Pertama",
+            "slug"   => "judul-hari-pertama",
+            "author" => "Anigato",
+            "body"   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, excepturi debitis et atque aut odio consequatur recusandae quo unde laborum magni sequi omnis voluptates tempore ipsum tenetur reprehenderit placeat vel aperiam animi? Magnam cupiditate, voluptate quis sapiente cumque earum similique reprehenderit distinctio iusto, quam suscipit recusandae possimus facere deleniti odit perferendis delectus temporibus at explicabo beatae dolor ipsa eligendi? Dolores exercitationem repellat autem? Modi debitis voluptatum impedit molestias, enim commodi labore dolor in maiores? Dolorem, placeat eum voluptatum doloremque nostrum doloribus voluptas repellendus neque officia exercitationem delectus sapiente itaque consequuntur ut vitae, eligendi deserunt nulla! Voluptas error aspernatur quasi! Minima."
+
+        ],
+        [
+            "title"  => "Hari Kedua",
+            "slug"   => "judul-hari-kedua",
+            "author" => "Khoerul",
+            "body"   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, excepturi debitis et atque aut odio consequatur recusandae quo unde laborum magni sequi omnis voluptates tempore ipsum tenetur reprehenderit placeat vel aperiam animi? Magnam cupiditate, voluptate quis sapiente cumque earum similique reprehenderit distinctio iusto, quam suscipit recusandae possimus."
+
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+//route ke posting blog 
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title"  => "Hari Pertama",
+            "slug"   => "judul-hari-pertama",
+            "author" => "Anigato",
+            "body"   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, excepturi debitis et atque aut odio consequatur recusandae quo unde laborum magni sequi omnis voluptates tempore ipsum tenetur reprehenderit placeat vel aperiam animi? Magnam cupiditate, voluptate quis sapiente cumque earum similique reprehenderit distinctio iusto, quam suscipit recusandae possimus facere deleniti odit perferendis delectus temporibus at explicabo beatae dolor ipsa eligendi? Dolores exercitationem repellat autem? Modi debitis voluptatum impedit molestias, enim commodi labore dolor in maiores? Dolorem, placeat eum voluptatum doloremque nostrum doloribus voluptas repellendus neque officia exercitationem delectus sapiente itaque consequuntur ut vitae, eligendi deserunt nulla! Voluptas error aspernatur quasi! Minima."
+
+        ],
+        [
+            "title"  => "Hari Kedua",
+            "slug"   => "judul-hari-kedua",
+            "author" => "Khoerul",
+            "body"   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, excepturi debitis et atque aut odio consequatur recusandae quo unde laborum magni sequi omnis voluptates tempore ipsum tenetur reprehenderit placeat vel aperiam animi? Magnam cupiditate, voluptate quis sapiente cumque earum similique reprehenderit distinctio iusto, quam suscipit recusandae possimus."
+
+        ],
+    ];
+
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] == $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Singgle Post",
+        "post"  => $new_post
+    ]);
 });
